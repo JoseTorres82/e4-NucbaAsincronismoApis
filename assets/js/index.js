efc47e0ac875fb9caaf83ef0f40a14320c4b0693
JoseTorres82 <josejtorres82@gmail.com>
2023-06-10 15:00:40 -0300

    e4HtmlCssListoJSokPreventDefaultOk

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,69 +1,65 @@
-const searchButton = document.getElementById('btn-buscar');
+const pokeBusquedaForm = document.getElementById('pokeBusquedaForm');
 const pokemonContainer = document.getElementById('pokemonContainer');
 
-searchButton.addEventListener('click', () => {
-    const cardnNumberInput = document.getElementById('inputNum');
-    const cardNumber = cardnNumberInput.value;
-    if (cardNumber === '') {
-        const errorCardHTML = `
+pokeBusquedaForm.addEventListener('submit', event => {
+  event.preventDefault(); 
+  const cardNumberInput = document.getElementById('inputNum');
+  const cardNumber = cardNumberInput.value;
+  if (cardNumber === '') {
+    const errorCardHTML = `
         <img src="/assets/img/notFound.png" alt="error-image">
-        <p> Por favor, ingresa un número de Pokémon válido.</p>
+        <p> Por favor, ingresa un número de carta válido.</p>
         `;
         pokemonContainer.innerHTML = errorCardHTML;
-        return;
-    }
-    fetchPokemonData(cardNumber);
-
-    cardnNumberInput.value ="";
+    return;
+  }
+  fetchPokemonData(cardNumber);
+  cardNumberInput.value ="";
 });
 
+function fetchPokemonData(pokemonNumber) {
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`;
 
-function fetchPokemonData(cardNumber) {
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${cardNumber}`;
-
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-            
-                throw new Error('Pokémon no encontrado');
-                
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayPokemonData(data);
-        })
-        .catch(error => {
-            showError(error.message);
-            const empyCardHTML = `
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Pokémon no encontrado');
+      }
+      return response.json();
+    })
+    .then(data => {
+      displayPokemonData(data);
+    })
+    .catch(error => {
+      showError(error.message);
+      const empyCardHTML = `
         <img src="/assets/img/emptyError.png" alt="error-image">
         <p> Pokémon no encontrado.</p>
         `;
         pokemonContainer.innerHTML = empyCardHTML;
-        });
+
+    });
 }
 
 function displayPokemonData(pokemonData) {
-    const pokemonName = pokemonData.name;
-    const pokemonTypes = pokemonData.types.map(type => type.type.name).join(', ');
-    const pokemonHeight = pokemonData.height / 10;
-    const pokemonWeight = pokemonData.weight / 10;
-    const pokemonImageUrl = pokemonData.sprites.front_default;
+  const pokemonName = pokemonData.name;
+  const pokemonTypes = pokemonData.types.map(type => type.type.name).join(', ');
+  const pokemonHeight = pokemonData.height / 10; 
+  const pokemonWeight = pokemonData.weight / 10; 
+  const pokemonImageUrl = pokemonData.sprites.front_default;
 
-    const pokemonCardHTML = `
-  <img src="${pokemonImageUrl}" alt="${pokemonName}">
+  const pokemonCardHTML = `
+    <img src="${pokemonImageUrl}" alt="${pokemonName}">
     <h2>${pokemonName}</h2>
     <p><strong>Tipo:</strong> ${pokemonTypes}</p>
     <p><strong>Altura:</strong> ${pokemonHeight} m</p>
     <p><strong>Peso:</strong> ${pokemonWeight} kg</p>
-    
   `;
 
-    pokemonContainer.innerHTML = pokemonCardHTML;
-
+  pokemonContainer.innerHTML = pokemonCardHTML;
 }
-function showError(errorMessage) {
-    const errorHTML = `<p class="error-message">${errorMessage}</p>`;
-    pokemonContainer.innerHTML = errorHTML;
 
+function showError(errorMessage) {
+  const errorHTML = `<p class="error-message">${errorMessage}</p>`;
+  pokemonContainer.innerHTML = errorHTML;
 }
